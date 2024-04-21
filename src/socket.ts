7efa15d7088634ed7eb4bfff5b5550a98b0a198d
merhmood <mahmudsuberu9@gmail.com>
2024-04-21 22:34:54 +0100

    fix: add api url to socket

--- src/socket.ts
+++ src/socket.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production"
-    ? (process.env.PROD_URL as string)
-    : "http://127.0.0.1:8000";
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8000"
+    : "https://api.omnihale.com";
 
 export const socket = io(URL);
